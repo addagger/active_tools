3f24421d8937509b800bd84566856f42fd749885
macintosh <macintosh@nyc.local>
2014-06-16 07:02:55 +0400

    custom_counter_cache updated

--- lib/active_tools/active_record/custom_counter_cache/instance_methods.rb
+++ lib/active_tools/active_record/custom_counter_cache/instance_methods.rb
@@ -12,10 +12,8 @@ module ActiveTools
         end
 
         def custom_counter_cache_before_destroy(assoc_name, reflection, assoc_mapping)
-          foreign_key = reflection.foreign_key.to_sym
-          unless destroyed_by_association && destroyed_by_association.foreign_key.to_sym == foreign_key
-            record = send(assoc_name)
-            if record && !self.destroyed?
+          unless destroyed_by_association && (destroyed_by_association.foreign_key.to_sym == reflection.foreign_key.to_sym)
+            if (record = send(assoc_name)) && !self.destroyed?
               ActiveRecord::CustomCounterCache.digger(self, record, assoc_mapping) do |parent, cache_column, value|
                 parent.class.update_counters(parent.id, cache_column => -value)
               end
@@ -27,17 +25,19 @@ module ActiveTools
           foreign_key  = reflection.foreign_key
           if (@_after_create_custom_counter_called ||= false)
             @_after_create_custom_counter_called = false
-          elsif send(:attribute_changed?, foreign_key) && !new_record? && defined?(reflection.klass.to_s.camelize)
-            model           = reflection.klass
-            foreign_key_was = attribute_was foreign_key
-            foreign_key     = attribute foreign_key
+          elsif !new_record? && ((send(:attribute_changed?, foreign_key) && defined?(reflection.klass.to_s.camelize))||
+                (reflection.polymorphic? && send(:attribute_changed?, reflection.foreign_type)))
+            model           = (attribute(reflection.foreign_type).try(:constantize) if reflection.polymorphic?)||reflection.klass
+            model_was       = (attribute_was(reflection.foreign_type).try(:constantize) if reflection.polymorphic?)||reflection.class
+            foreign_key_was = attribute_was(foreign_key)
+            foreign_key     = attribute(foreign_key)
 
             if foreign_key && model.respond_to?(:increment_counter) && to_increment = model.find_by_id(foreign_key)
               ActiveRecord::CustomCounterCache.digger(self, to_increment, assoc_mapping) do |parent, cache_column, value|
                 parent.class.update_counters(parent.id, cache_column => value)
               end
             end
-            if foreign_key_was && model.respond_to?(:decrement_counter) && to_decrement = model.find_by_id(foreign_key_was)
+            if foreign_key_was && model_was.respond_to?(:decrement_counter) && to_decrement = model_was.find_by_id(foreign_key_was)
               ActiveRecord::CustomCounterCache.digger(self, to_decrement, assoc_mapping) do |parent, cache_column, value|
                 parent.class.update_counters(parent.id, cache_column => -value)
               end
