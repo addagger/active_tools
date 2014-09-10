6c406f8cda926156705198004fce3741c81418d9
macintosh <macintosh@nyc-5.local>
2014-09-10 17:39:19 +0400

    runtime_caching remake

--- lib/active_tools/core_extension/runtime_caching.rb
+++ lib/active_tools/core_extension/runtime_caching.rb
@@ -3,28 +3,14 @@ module ActiveTools
     module RuntimeCaching
       def acts_as_runtime_caching(*args)
         options = args.extract_options!
-        accessors = args
-        thread_key = options[:thread_key]||::SecureRandom.urlsafe_base64(nil, false).to_sym
-        send(:include, ::Singleton)
-        container = Class.new
-        container.send(:include, ::ActiveSupport::Configurable)
-        container.instance_eval do
-          config_accessor *accessors
-        end
-        define_method :config do
-          ::Thread.current[thread_key] ||= container.new
-        end
-        define_method :"config=" do |value|
-          ::Thread.current[thread_key] = value
-        end
-        accessors.each do |method|
+        args.each do |method|
           instance_eval <<-DELEGATORS
             def #{method}
-              instance.config.#{method}
+              ::Thread.current[:#{method}]
             end
 
             def #{method}=(value)
-              instance.config.#{method} = (value)
+              ::Thread.current[:#{method}] = (value)
             end
           DELEGATORS
         end
