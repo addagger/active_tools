21814a3564af468bf2bbf668533ec9411875d219
macintosh <macintosh@nyc.local>
2015-10-25 21:44:46 +0300

    field_name added

--- lib/active_tools/action_pack/action_view.rb
+++ lib/active_tools/action_pack/action_view.rb
@@ -1,6 +1,7 @@
 require 'active_tools/action_pack/action_view/tag_attributes'
 require 'active_tools/action_pack/action_view/perform_as_tree'
 require 'active_tools/action_pack/action_view/uniq_content_for'
+require 'active_tools/action_pack/action_view/field_name'
 
 module ActiveTools
   module ActionPack
