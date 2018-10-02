2e83cea3cde7c06d9ff2e32abb2fff3578fc29b0
macintosh <macintosh@nyc.local>
2018-10-02 19:02:59 +0300

    0.2.4

--- lib/active_tools/activerecord.rb
+++ lib/active_tools/activerecord.rb
@@ -4,6 +4,7 @@ require 'active_tools/active_record/custom_counter_cache'
 require 'active_tools/active_record/with_permalink'
 require 'active_tools/active_record/polymorphic_conditions'
 require 'active_tools/active_record/filter_assign'
+require 'active_tools/active_record/commit_and_touch'
 
 module ActiveTools
   module ActiveRecord
